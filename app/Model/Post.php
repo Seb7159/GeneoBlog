6769e18c5a418a41852c2bc45834fad9504ddbb4
sxs1476 <sxs1476@cs.bham.ac.uk>
2018-01-15 13:42:10 +0000

    Added user authentication
    First part out of the three is done.

---
+++ app/Model/Post.php
@@ -0,0 +1,7 @@
+<?php
+
+public function isOwnedBy($post, $user) {
+    return $this->field('id', array('id' => $post, 'user_id' => $user)) !== false;
+}
+
+?> 
---
+++ app/View/Posts/index.ctp
@@ -0,0 +1,3 @@
+<div class="posts">
+Work in progress! 
+</div> 
